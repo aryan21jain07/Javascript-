4635071f4839fbecfdcc86e411b5cbeb1a06a7b5
aryan21jain07 <aryan21jain07@gmail.com>
2025-11-01 06:27:27 +0000

    updated

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,20 @@
+let score = 33
+
+
+console.log(typeof score);
+
+//type conversion
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" =>  33, "aryan" => NaN , "true" => 1 , "false" => 0 , null => 0 , undefined => NaN, " " => 0 , "33abc" => NaN , "  33  " => 33 
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+let isLoggedIn = "aryan"
--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -4,4 +4,21 @@
 
 // code readibility must be there 
 
+let name ="aryan";
+let age = 22;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
 
+
+//number -> 2 to the power of 53
+//string -> sequence of characters
+//boolean -> true or false
+//undefined -> value not assigned
+//null -> empty value   
+
+// symbol -> unique identifier
+
+//object -> key value pairs
+//array -> ordered list of items
+//function -> perform a task or calculate a value

ec34c1a2f2ba998e77d5fba5f4a95336cc23b423
aryan21jain07 <aryan21jain07@gmail.com>
2025-11-01 05:55:56 +0000

    test commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,22 @@
 console.log("aryan is a good man")
+
+// const accountId =  14453;
+let accountEmail = "aryan@gmail.com";
+var accountPassword =" 12345"
+accountCity = "jaipur"
+
+
+accountId =2
+
+accountEmail = "aryan21@gmail.com"
+accountPassword ="212121"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+
+console.log([accountEmail,accountPassword,accountCity,accountId])
+
+/*
+prefer not to  use var because of issue in block scope and functional scope 
+*/
+
